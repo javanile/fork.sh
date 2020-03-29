5985cd7cb011846e53295b08fd440975db43203e
Francesco Bianco <info.francescobianco@gmail.com>
2020-03-29 17:06:25 +0200

    Before fork updates

--- fork.sh
+++ fork.sh
@@ -102,7 +102,7 @@ parse () {
 }
 
 main () {
-    git commit -am "Before fork updates" && true > /dev/null 2>&1
+    git commit -am "Before fork updates" > /dev/null 2>&1 && true
     #parse LOCAL ${workdir} ${workdir}
     #debug "Commit Forkfile retrieved updates"
     #git commit -am "Fork updates done"
