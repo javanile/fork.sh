ed0d9d946a7fb9a4e631afc9f5b3632ff7f6d44d
Francesco Bianco <info.francescobianco@gmail.com>
2020-08-31 11:49:33 +0200

    Update fork.sh

--- fork.sh
+++ fork.sh
@@ -90,7 +90,7 @@ case "$(uname -s)" in
         escape='\x1B'
         ;;
     Linux|*)
-        getopt=/usr/bin/getopt
+        [ -x /bin/getopt ] && getopt=/bin/getopt || getopt=/usr/bin/getopt
         escape='\e'
         ;;
 esac
